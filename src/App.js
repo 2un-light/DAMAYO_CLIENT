1f1c4b9a12e62df35b8de9f31621d4f5e30b108c
차은영 <sts07129@naver.com>
2024-06-11 16:24:30 +0900

    [UI/UX] Login 페이지 구현

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<Home/>}/>
+          <Route path="/login" element={<Login/>}/>
         </Routes>
       </div>
     </Router>
